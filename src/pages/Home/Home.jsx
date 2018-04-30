b562419c8dfb468e980c78be399d4878b973b0b5
Hannah Nordgren <hey@hereshannahs.info>
2018-04-29 22:46:51 -0400

    [WIP] starting to add in redux

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { push } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Categories from '../../components/Categories/Categories'
 
 const Home = () => {
@@ -10,4 +13,17 @@ const Home = () => {
   );
 }
 
-export default Home;
+// setting up for now, no idea what the state of a joke is yet.
+const mapStateToProps = state => {
+  return {
+    jokes: state,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    changePage: () => push('/about'),
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
