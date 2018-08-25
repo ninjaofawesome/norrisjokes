145cfc967d00757884571a1133418c39ab13eb8c
Hannah Nordgren <hey@hereshannahs.info>
2018-08-25 18:12:23 -0400

    [WIP] upgraded to CSS modules, want to map output into larger bundled file

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import '../../index.css';
+
+import styles from './App.css';
 
 class App extends Component {
   constructor() {
@@ -55,7 +56,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className='app'>
+      <div className={styles.app}>
         <p>Hello World</p>
         <p>{ this.state.randomJoke }</p>
         <ul>
