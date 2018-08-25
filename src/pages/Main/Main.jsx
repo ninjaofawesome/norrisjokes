ac6297f6e5d97e712324c5fe04ff9e225bb7e1ea
Hannah Nordgren <hey@hereshannahs.info>
2018-08-25 14:44:49 -0400

    [CLEANUP] just focusing on API for now

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from '../Home/Home';
-import About from '../About/About';
 
 const Main = () => (
   <div className='main'>
-    <Switch>
-      <Route exact path='/home' component={Home}/>
-      <Route path='/about' component={About}/>
-    </Switch>
+    Hello world main
   </div>
 )
 
