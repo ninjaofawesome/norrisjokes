d13ca1d895fdd58fdc33475162f4a08770afdf51
Hannah Nordgren <hey@hereshannahs.info>
2018-09-11 21:39:59 -0400

    [WIP] connected child routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,80 +1,22 @@
-import React, { Component } from 'react';
-import Title from '../../components/Title/Title';
-import { handleResponse } from '../../utils/helperFunctions.js';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      randomJoke: '',
-      categories: [],
-    };
-
-    this.randomJokeRequest = this.randomJokeRequest.bind(this);
-    this.getJokeCategories = this.getJokeCategories.bind(this);
-    this.showJokeCategories = this.showJokeCategories.bind(this);
-  }
-
-  randomJokeRequest() {
-    fetch('https://api.chucknorris.io/jokes/random')
-    .then(handleResponse)
-    .then(data => {
-      this.setState({ randomJoke: data.value });  
-    })
-    .catch(error => console.log(error));
-  }
-
-  getJokeCategories() {
-    fetch('https://api.chucknorris.io/jokes/categories')
-    .then(handleResponse)
-    .then(data => {
-      this.setState({ categories: data})
-    })
-    .catch(error => console.log(error));
-  }
-
-  componentDidMount() {
-    this.randomJokeRequest();
-    this.getJokeCategories();
-  }
-
-  showJokeCategories() {
-    const {
-      categories,
-    } = this.state;
-
-    if (categories.length > 0) {
-      return categories.map((item, index) => (
-        <li key={`category-${index}`}>{item}</li>
-      ))
-    }
-  }
-
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appTitleContainer}>
-          <Title
-            type='bold'
-            header={1}
-            size='3em'
-            copy='NorrisJokes'
-          />
-        </div>
-        <div className={styles.appTextContainer}>
-          <p class={styles.appJoke}>{ this.state.randomJoke }</p>
-        </div>
-        <div classname={styles.appListContainer}>
-          <ul className={styles.appList}>
-            {this.showJokeCategories()}
-          </ul>
-        </div>
-      </div>
-    );
-  }
+const App = props => {
+  return (
+    <div className={styles.appContainer}>
+      {props.children}
+    </div>
+  );
 }
+
+App.propTypes = {
+  children: PropTypes.node,
+};
+
+App.defaultProps = {
+  children: <div />,
+};
 
 export default App;
