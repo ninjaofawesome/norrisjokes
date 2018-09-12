3020b1f41c02e4dede52fbd6eb2a5ac8139ec629
Hannah Nordgren <hey@hereshannahs.info>
2018-09-12 08:42:04 -0400

    [WIP] jokes not fetching

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { createBrowserRouter } from 'found';
 import routeConfig from './pages/Routes.jsx';
 
-import App from './pages/App/App.jsx';
-import Categories from './pages/Categories/Categories.jsx';
-
 import registerServiceWorker from './registerServiceWorker';
 
 const BrowserRouter = createBrowserRouter({ routeConfig })
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Title from '../../components/Title/Title';
 import Button from '../../components/Button/Button';
+import { handleResponse } from '../../utils/helperFunctions.js';
 
 import styles from './Main.css';
 
-const MainPage = () => (
-  <div className={styles.mainPageContainer}>
-    <div className={styles.mainTitleContainer}>
-      <Title
-        header={1}
-        type='bold'
-        size='3em'
-        copy='NorrisJokes'
-      />
-    </div>
-    <div className={styles.mainContentContainer}>
-      <p>This is the main page of the app.  Click to go somewhere</p>
-      <div className={styles.mainButtonWrapper}>
-        <Button
-          type='navigation'
-          location='/categories'
-          content='Categories'
-          color='black'
-        />
+class MainPage extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      randomJoke: '',
+    };
+
+    this.randomJokeRequest = this.randomJokeRequest.bind(this);
+  }
+
+  randomJokeRequest() {
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(handleResponse)
+    .then(data => {
+      this.setState({ randomJoke: data.value });  
+    })
+    .catch(error => console.log('something went wrong!', error));
+  }
+
+  componentDidMount() {
+    this.randomJokeRequest();
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className={styles.mainPageContainer}>
+        <div className={styles.mainTitleContainer}>
+          <Title
+            header={1}
+            type='bold'
+            size='3em'
+            copy='NorrisJokes'
+          />
+        </div>
+        <div className={styles.mainContentContainer}>
+          <Title
+            header={2}
+            type='light'
+            size='1.5em'
+            copy='Chuck Norris jokes for all occasions!'
+          />
+          <p className={styles.jokeText}>{this.state.randomJoke}</p>
+          <p>This is the main page of the app.  Click to go somewhere</p>
+          <div className={styles.mainButtonWrapper}>
+            <Button
+              type='navigation'
+              location='/categories'
+              content='Categories'
+              color='black'
+            />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 
 export default MainPage;
