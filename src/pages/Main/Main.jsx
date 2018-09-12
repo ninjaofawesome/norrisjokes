88bee93af70f34ed4c48e5c674110853e78add60
Hannah Nordgren <hey@hereshannahs.info>
2018-09-11 22:20:46 -0400

    [WIP] setting up index page for now, all breakpoints

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Link } from 'found';
+import Title from '../../components/Title/Title';
+import Button from '../../components/Button/Button';
+
+import styles from './Main.css';
 
 const MainPage = () => (
-  <div>
-    I am a Main page
-    <Link to='/'>Home</Link>
-    <Link to='/categories'>Home</Link>
+  <div className={styles.mainPageContainer}>
+    <div className={styles.mainTitleContainer}>
+      <Title
+        header={1}
+        type='bold'
+        size='3em'
+        copy='NorrisJokes'
+      />
+    </div>
+    <div className={styles.mainContentContainer}>
+      <p>This is the main page of the app.  Click to go somewhere</p>
+      <div className={styles.mainButtonWrapper}>
+        <Button
+          type='navigation'
+          location='/categories'
+          content='Categories'
+          color='black'
+        />
+      </div>
+    </div>
   </div>
 );
 
